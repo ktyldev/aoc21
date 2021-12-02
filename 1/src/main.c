99a03770e3caedbb02cdaea4484050ea244720bd
ktyl <me@ktyl.dev>
2021-12-02 01:22:13 +0000

    second gold star and also gitignore

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 const char* inputFilename = "input";
 
@@ -6,20 +7,38 @@ int main()
 {
     FILE* fp;
     char buf[255];
+
     int lastReading = -1;
     int currentReading = -1;
     int increases = 0;
+    int reading[3];
 
-    // read input
+    // open input file for reading
     fp = fopen(inputFilename, "r");
-    fscanf(fp, "%s", buf);
-    currentReading = atoi(buf);
-    printf("%d\n", currentReading);
 
+    // load up first three positions
+    fscanf(fp, "%s", buf);
+    reading[0] = atoi(buf);
+    fscanf(fp, "%s", buf);
+    reading[1] = atoi(buf);
+    fscanf(fp, "%s", buf);
+
+    // off we loop
     while (fscanf(fp, "%s", buf) == 1)
     {
+        // move elements over and drop the first one
+        reading[0] = reading[1];
+        reading[1] = reading[2];
+
+        // read in new element
+        reading[2] = atoi(buf);
+
         lastReading = currentReading;
-        currentReading = atoi(buf);
+        currentReading = 0;
+        for (int i=0; i<3; i++)
+        {
+            currentReading += reading[i];
+        }
 
         printf("%d", currentReading);
 
